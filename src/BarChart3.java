f4d0f74998a2847f99af29804be5499327bb4f3e
Lola1590 <Lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-11 11:54:27 -0400

    Work from school

--- src/BarChart3.java
+++ src/BarChart3.java
@@ -2,6 +2,7 @@ import TurtleGraphics.SketchPadWindow;
 import TurtleGraphics.StandardPen;
 import TurtleGraphics.Pen;
 import java.awt.Color;
+import java.util.Scanner;
 
 
 public class BarChart3 {
@@ -10,10 +11,19 @@ public class BarChart3 {
     public static void main(String[] args) {
         Pen p = new StandardPen(new SketchPadWindow(800,600));
         Penbar pb1,pb2,pb3,pb4;
+        Scanner scan = new Scanner(System.in);
         
-        //makeBar(p, pb1, -350,200, pb1.width, Color.blue); 
         
         
+        
+      //  makeBar(p, pb1, -350,200, pb1.width, Color.blue); 
+       // makeBar(p, pb2, -350,100, pb1.width, Color.red);  
+       // makeBar(p, pb3, -350,0, pb1.width, Color.green); 
+       // makeBar(p, pb4, -350,-100, pb1.width, Color.orange); 
+               
+    } 
+        public static void makeBar(Pen p, Penbar pb, int x, int y, int w, Color c){
+       // pb.xloc;
         
     }
     
