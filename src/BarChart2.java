1bdb3ea414849ab6a24121f8b3c4bced631aaebf
Lola1590 <Lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-12 12:49:00 -0400

    Updated work from school

--- src/BarChart2.java
+++ src/BarChart2.java
@@ -4,40 +4,19 @@ public class BarChart2 {
 
     public static void main(String[] args) {
        Console c = new Console();
-       Bar b1 = new Bar();
-       Bar b2 = new Bar();
-       Bar b3 = new Bar();
-       Bar b4 = new Bar();
-       //bar 1       
-       c.print("Enter height of bar1> ");
-         
-       b1.xloc = 200;
-       b1.height =300;
-       b1.col = Color.red;
-       //bar 2
-       
-      c.print("Enter height of bar2>  ");
-       
-       b2.xloc = 300;
-       b2.height= 50;
-       b2.col = Color.green;
-       //bar3
-       c.print("Enter the height of bar3> ");
-       b3.xloc = 400;
-       b3.height= 50;
-       b3.col = Color.blue;
-       //bar 4
-       c.print("Enter the height of bar4> ");
-       b4.xloc = 500;
-       b4.height= 50;
-       b4.col = Color.yellow;
-       
-       
-       //draw the bars
-       b1.draw(c);
-       b2.draw(c);
-       b3.draw(c);
-       b4.draw(c);
+       //array of 4 colors for 4 bars
+       Color col[] = {Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE};
+       Bar b[] = new Bar[4]; //empty array for 4 bars
+        for (int x = 0; x < 4; x++) {
+            b[x] = new Bar(); //new actual bar at location x in array
+            c.print("Enter bar " + (x+1) + " height >");
+            //assign bar height, xloc, color
+            b[x].height = c.readInt();
+            b[x].xloc = 50 + (100*x);
+            b[x].col = col[x];
+            //draw
+            b[x].draw(c);
+        }
        
        
        
