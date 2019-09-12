9d328cd49504d60f0fde243f54a56fc9b26c7bb5
Lola1590 <Lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-12 12:35:49 -0400

    Updated work from school

--- src/Momentum.java
+++ src/Momentum.java
@@ -5,26 +5,18 @@ public class Momentum {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double velocity, mass, momentum;
-        
-        //mass
-        System.out.println("Enter a number for mass in kg's: ");
+        double velocity, mass, momentum;    
+        System.out.print("Enter a Mass in kg's: ");
         mass = scan.nextDouble();
-        System.out.format("You entered the number %.2f.\n", mass);
-        //velocity
-        System.out.println("Enter a number for velocity in m/s");
-        velocity = scan.nextDouble();
-        System.out.format("You entered the number %.2f.\n", velocity);
-        //momentum 
-        
-        while(true){
-            momentum = scan.nextDouble();
-       //     momentum = mass*velocity
-        System.out.format("Your objects momentum will be %.2f . \n",momentum);
+        System.out.format("Enter a Velocity in m/s: ");       
+        velocity = scan.nextDouble();      
+        //momentum calculation 
+        momentum = mass*velocity;
+        System.out.format("Your objects momentum will be %.2f\n",momentum);
       
                     
-        //System.out.format("Your objects momentum will be %.2f . \n",momentum);
-        }
+        
+        
         
     }
 
